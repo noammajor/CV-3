db7f7ab0859b0d579cb7308e3fd265b74a5f22f4
noammajor <noam.major@campus.technion.ac.il>
2026-02-08 11:57:50 +0200

    fix

--- icv83551/datasets/get_coco_dataset.sh
+++ icv83551/datasets/get_coco_dataset.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-if [ ! -d "coco_captioning" ]; then
-    wget "http://cs231n.stanford.edu/coco_captioning.zip"
-    unzip coco_captioning.zip
-    rm coco_captioning.zip
-fi
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ac1089d38b4875b422f01f5f51e8fe77e1bbbcead6aa76e6b9d4d110eee2fc5
+size 165
--- icv83551/datasets/get_imagenet_val.sh
+++ icv83551/datasets/get_imagenet_val.sh
@@ -1,1 +1,3 @@
-wget http://cs231n.stanford.edu/imagenet_val_25.npz
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e5506afff66fa8412c1cbe635b73a203618e2af7cb26781f73c57c83dd22c0f
+size 52
